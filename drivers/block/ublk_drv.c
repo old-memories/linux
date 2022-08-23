62ef4e0db61078098172dc6fb166de84a3d21820
ZiyangZhang <ZiyangZhang@linux.alibaba.com>
2022-08-23 17:36:51 +0800

    ublk_drv: add pr_devel() to prepare for recovery feature
    Recovery feature needs to correctly complete all old ioucmds. So print
    some message for one ioucmd to track its status.
    
    After io_uring ctx is freed, the /dev/ublkcX file can be released. Print
    while opening/releasing the file struct to make sure there is no
    resource leakage.
    
    Signed-off-by: ZiyangZhang <ZiyangZhang@linux.alibaba.com>

--- drivers/block/ublk_drv.c
+++ drivers/block/ublk_drv.c
@@ -572,6 +572,9 @@ static void ublk_complete_rq(struct request *req)
 	struct ublk_io *io = &ubq->ios[req->tag];
 	unsigned int unmapped_bytes;
 
+	pr_devel("%s complete req: qid %d tag %d io_flags %d\n",
+			__func__, ubq->q_id, req->tag, io->flags);
+
 	/* failed read IO if nothing is read */
 	if (!io->res && req_op(req) == REQ_OP_READ)
 		io->res = -EIO;
@@ -619,6 +622,9 @@ static void ubq_complete_io_cmd(struct ublk_io *io, int res)
 	 * We can only accept io cmd in case of being not active.
 	 */
 	io->flags &= ~UBLK_IO_FLAG_ACTIVE;
+
+	pr_devel("%s: complete: op %d, res %d io_flags %x\n",
+			__func__, io->cmd->cmd_op, res, io->flags);
 
 	/* tell ublksrv one io request is coming */
 	io_uring_cmd_done(io->cmd, res, 0);
@@ -816,6 +822,7 @@ static int ublk_ch_open(struct inode *inode, struct file *filp)
 	if (test_and_set_bit(UB_STATE_OPEN, &ub->state))
 		return -EBUSY;
 	filp->private_data = ub;
+	pr_devel("%s: /dev/ublkc%d opened.\n", __func__, ub->dev_info.dev_id);
 	return 0;
 }
 
@@ -824,6 +831,7 @@ static int ublk_ch_release(struct inode *inode, struct file *filp)
 	struct ublk_device *ub = filp->private_data;
 
 	clear_bit(UB_STATE_OPEN, &ub->state);
+	pr_devel("%s: /dev/ublkc%d released.\n", __func__, ub->dev_info.dev_id);
 	return 0;
 }
 
@@ -942,6 +950,8 @@ static void ublk_daemon_monitor_work(struct work_struct *work)
 		if (ubq->ubq_daemon && ubq_daemon_is_dying(ubq) && !ubq->force_abort) {
 			struct request_queue *q = ub->ub_disk->queue;
 
+			pr_devel("%s: find dying ubq_daemon: qid %d\n",
+					__func__, ubq->q_id);
 			ubq->force_abort = true;
 
 			/* ensure that all ublk_queue_rq() calls see force_abort */
@@ -1043,12 +1053,16 @@ static void ublk_mark_io_ready(struct ublk_device *ub, struct ublk_queue *ubq)
 	mutex_lock(&ub->mutex);
 	ubq->nr_io_ready++;
 	if (ublk_queue_ready(ubq)) {
+		pr_devel("%s: ubq %d ready\n", __func__, ubq->q_id);
 		ubq->ubq_daemon = current;
 		get_task_struct(ubq->ubq_daemon);
 		ub->nr_queues_ready++;
 	}
-	if (ub->nr_queues_ready == ub->dev_info.nr_hw_queues)
+	if (ub->nr_queues_ready == ub->dev_info.nr_hw_queues) {
+		pr_devel("%s: all queues(%d) ready\n",
+				__func__, ub->dev_info.nr_hw_queues);
 		complete_all(&ub->completion);
+	}
 	mutex_unlock(&ub->mutex);
 }
 
